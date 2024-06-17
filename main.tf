6b86f4c0c48a04cd5f9142e327849b78155c9cba
Krupananda Reddy <krupaops.dev@gmail.com>
2024-06-17 10:54:39 +0530

    launch EC2

--- main.tf
+++ main.tf
@@ -3,3 +3,11 @@ resource "local_file" "sample" {
     content = "my first terraform file with local resource \n"
   
 }
+
+
+resource "aws_instance" "tf-launch" {
+    ami = "ami-04b70fa74e45c3917"
+    instance_type = "t2.micro"
+    
+  
+}
